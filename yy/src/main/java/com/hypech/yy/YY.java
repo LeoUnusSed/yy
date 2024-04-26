cc4a84eac9ef383308d9173806acad3e27932dbd
aiXpertLab <aiXpertLab@gmail.com>
2024-04-26 19:08:57 -0400

    skeleton of SpringBoot

---
+++ yy/src/main/java/com/hypech/yy/YY.java
@@ -0,0 +1,13 @@
+package com.hypech.yy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YY {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YY.class, args);
+    }
+
+}
---
+++ yy/src/test/java/com/hypech/yy/YYTests.java
@@ -0,0 +1,13 @@
+package com.hypech.yy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YYTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
